b06a4f100da1b19c037e8f3a5e270c57d1ce65c8
Fidel G Vazquez <fidgvaz@gmail.com>
2025-11-09 14:25:38 -0800

    feat(room): add AppDatabase singleton

--- app/src/main/java/edu/csumb/hw04_gimlog/data/AppDatabase.java
+++ app/src/main/java/edu/csumb/hw04_gimlog/data/AppDatabase.java
@@ -1,13 +1,31 @@
 package edu.csumb.hw04_gimlog.data;
 
+import android.content.Context;
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(
-        entities = {GymLog.class},
-        version = 1,
-        exportSchema = true
-)
+@Database(entities = {GymLog.class}, version = 1, exportSchema = true)
 public abstract class AppDatabase extends RoomDatabase {
+    private static volatile AppDatabase INSTANCE;
+
     public abstract GymLogDao gymLogDao();
+
+    public static AppDatabase getInstance(Context ctx) {
+        if (INSTANCE == null) {
+            synchronized (AppDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(
+                                    ctx.getApplicationContext(),
+                                    AppDatabase.class,
+                                    "gymLog.db"
+                            )
+                            .allowMainThreadQueries() // OK for homework/simplicity
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
+
